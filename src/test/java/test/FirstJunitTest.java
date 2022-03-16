332304840015af3da8369dee16a4ceea45a924ce
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-03-17 01:29:21 +0300

    work resolution 700x1080

--- src/test/java/test/FirstJunitTest.java
+++ src/test/java/test/FirstJunitTest.java
@@ -22,18 +22,18 @@ public class FirstJunitTest {
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        String user = System.getProperty("user");
-        String password = System.getProperty("password");
-        String remoteBrowser = System.getProperty("remoteBrowser");
+        //String user = System.getProperty("user");
+        //String password = System.getProperty("password");
+        //String remoteBrowser = System.getProperty("remoteBrowser");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "700x1080";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
         capabilities.setCapability("enableVideo",true);
         Configuration.browserCapabilities = capabilities;
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //Configuration.remote = "https://" + user + ":" + password + "@" + remoteBrowser;
-        Configuration.remote = "https://" + user + ":" + password + remoteBrowser;
+        //Configuration.remote = "https://" + user + ":" + password + remoteBrowser;
     }
 
     @AfterAll
